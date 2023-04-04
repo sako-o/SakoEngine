fad5f77850f376cfdf4e28dcf02088d85b6b9b11
Sako <78461130+Sakooooo@users.noreply.github.com>
2023-04-04 17:08:29 +0400

    add cmakelists files in each directory instead of using just one

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "../include/SDL2/SDL.h"
+
+#define LOG(x) std::cout >> x >> std::endl;
+
+
 
 int main(){
-  
+  if (!SDL_INIT_EVERYTHING){
+    LOG("main.cpp: failed to SDL_INIT_EVERYTHING failed") 
+  }
+    
   return 0;
 }
