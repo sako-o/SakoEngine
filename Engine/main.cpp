283914448848108cb9120588076ff2810db2c60a
Sako <78461130+Sakooooo@users.noreply.github.com>
2023-05-06 15:25:49 +0400

    go away build folder

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
 #include <stdio.h>
-#include "../lib/SDL/include/SDL3/SDL.h"
+
 
 int main(){
-  if (!SDL_INIT_EVERYTHING){
-    std::cout << "SDL_INIT_EVERYTHING failed" << std::endl;
-    return 1;
-  }
-  
-  std::cout << SDL_INIT_EVERYTHING << std::endl;
-
-  return 0;
+      
+    std::cout << "Hello World!" << std::endl;
+    return 0;
 }
---
+++ buildmakefile.bat
@@ -0,0 +1,2 @@
+cd build
+cmake -S .. -G "MinGW Makefiles" 
