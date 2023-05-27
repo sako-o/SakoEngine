8e2dda8c7d0d1ea723f2a60de578603f6f7b2412
Sako <78461130+Sakooooo@users.noreply.github.com>
2023-05-27 18:34:25 +0400

    loop works

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -1,10 +1,12 @@
 #include "SDL_error.h"
+#include "SDL_events.h"
 #include "SDL_init.h"
 #include "SDL_log.h"
 #include "SDL_timer.h"
 #include "SDL_video.h"
 #include <SDL.h>
 #include <SDL_image.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[]) {
   // init everything
@@ -15,11 +17,9 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
+  bool isRunning = true;
+
   // Window to place renderer in
-  //
-  // https://wiki.libsdl.org/SDL3/SDL_CreateWindow
-  //
-  //
   SDL_Window *window =
       SDL_CreateWindow("SakoEngine", 800, 800, SDL_WINDOW_OPENGL);
 
@@ -28,7 +28,21 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  SDL_Delay(3000);
+  // pauses execution in miliseconds
+  //  SDL_Delay(3000);
+
+  // tells SDL2 that the game
+  while (isRunning) {
+    // TODO(sako) think of a better name for this
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+      switch (event.type) {
+      default:
+        printf("SDL2 is waiting for events\n");
+      case SDL_EVENT_QUIT:
+        break;
+      }
+  }
 
   SDL_DestroyWindow(window);
 
