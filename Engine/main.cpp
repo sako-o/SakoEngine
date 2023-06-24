2449bbacb4863a365655a9006a0a0fdf78cd199f
Sako <78461130+Sakooooo@users.noreply.github.com>
2023-06-24 12:38:18 +0400

    i changed absolutely nothing

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -2,10 +2,10 @@
 #include "SDL_events.h"
 #include "SDL_init.h"
 #include "SDL_log.h"
-#include "SDL_timer.h"
 #include "SDL_video.h"
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_timer.h>
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
