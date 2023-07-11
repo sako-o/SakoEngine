eb6ca7fd3005cab42e76e9c74fdf88ac81755fa9
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-11 06:54:46 +0300

    tired will figur out later

--- shell.nix
+++ shell.nix
@@ -1,9 +1,22 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-     nativeBuildInputs = with pkgs; [
-      xorg.libX11
-      xorg.libX11.dev
-      xorg.libxcb.dev
+# default.nix
+with import <nixpkgs> {};
+stdenv.mkDerivation rec {
+    name = "dev-environment"; # Probably put a more meaningful name here
+    nativeBuildInputs = [
       pkg-config
+      gcc
+      gnumake
     ];
+    buildInputs = [ 
+      xorg.libX11.dev 
+      xorg.libXxf86vm.dev
+      xorg.libxkbfile.dev
+      xorg.libXv.dev
+      xorg.libxrandr.dev
+      zlib
+    ];
+
+    #LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+    X11_X11_INCLUDE_PATH = "${pkgs.xorg.libX11.dev}/include";
+    X11_X11_LIB = "${pkgs.xorg.libX11.dev}/lib";
 }
