096d56ffd114b4c907d9498b323640c742c01cc1
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-10 23:44:06 +0300

    flake + direnv

--- flake.nix
+++ flake.nix
@@ -1,12 +1,69 @@
 {
   description = "SakoEngine flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  };
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: {
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    nativeBuildInputs = with pkgs; [
+      clang-tools
+      pkg-config
+      cmake
+      gnumake
+    ];
+    buildInputs = with pkgs; [
+      ## XORG ##
+      xorg.libX11
+      xorg.libXrandr
+      xorg.libXrender
+      xorg.libXScrnSaver
+      xorg.libXext
+      xorg.libXft
+      xorg.libXpm.out
+      xorg.libXrandr
+      xorg.libXrender
+      xorg.libXcursor
+      xorg.xinput
+      xorg.libXi
+      xorg.libICE
+      xorg.xorgproto
+      xorg.libXinerama
+      xorg.libXxf86vm
+      ## END XORG ##
+      ## WAYLAND ##
+      ## END WAYLAND ##
+      # Nvidia Proprietary Linux
+      libdrm
+      mesa
+      # OpenGL support for windows
+      libGL
+      # vulkan for linux
+      vulkan-headers
+      vulkan-loader
+      vulkan-tools
+      vulkan-validation-layers
+      # We build SDL from source to get to
+      # SDL3, so we don't need this
+      SDL2.dev
+      SDL2_image
+      # Extras go here
+      zlib
+      libzip
+      udev
+      libiconv
+      # audio
+      libpulseaudio
+      pipewire
+    ];
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        nativeBuildInputs
+        buildInputs
+      ];
+      # ...
+    };
   };
 }
