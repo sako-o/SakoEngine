490a535ae86720d16738e43df8cb2f0de4df1aba
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-10 23:35:45 +0300

    brb'

--- flake.nix
+++ flake.nix
@@ -1,11 +1,12 @@
 {
-  description = "A very basic flake";
+  description = "SakoEngine flake";
 
-  outputs = { self, nixpkgs }: {
-
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
   };
 }
