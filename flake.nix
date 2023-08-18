b6467d4639c6b0c0842f9fb06f2dc1d7daca8031
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-18 13:28:19 +0000

    we

--- flake.nix
+++ flake.nix
@@ -14,48 +14,9 @@
       gnumake
     ];
     buildInputs = with pkgs; [
-      ## XORG ##
-      xorg.libX11
-      xorg.libXrandr
-      xorg.libXrender
-      xorg.libXScrnSaver
-      xorg.libXext.dev
-      xorg.libXft
-      xorg.libXpm.out
-      xorg.libXrandr
-      xorg.libXrender
-      xorg.libXcursor
-      xorg.xinput
-      xorg.libXi
-      xorg.libICE
-      xorg.xorgproto
-      xorg.libXinerama
-      xorg.libXxf86vm
-      ## END XORG ##
-      ## WAYLAND ##
-      ## END WAYLAND ##
-      # Nvidia Proprietary Linux
-      libdrm
-      mesa
-      # OpenGL support for windows
-      libGL
-      # vulkan for linux
-      vulkan-headers
-      vulkan-loader
-      vulkan-tools
-      vulkan-validation-layers
-      # We build SDL from source to get to
-      # SDL3, so we don't need this
+      # SDL2
       SDL2.dev
       SDL2_image
-      # Extras go here
-      zlib
-      libzip
-      udev
-      libiconv
-      # audio
-      libpulseaudio
-      pipewire
     ];
   in {
     devShells.${system}.default = pkgs.mkShell {
