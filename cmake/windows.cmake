89b55ba7755b239f1532969d400e517f72c12aee
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-19 16:11:51 +0000

    we

---
+++ cmake/windows.cmake
@@ -0,0 +1,28 @@
+# Sample toolchain file for building for Windows from an NixOS Linux system.
+#
+# Typical usage:
+#    *) add package pkgs.pkgsCross.mingwW64.buildPackages.gcc
+#    *) mkdir buildMingw64 && cd buildMingw64
+#    *) make windows
+#
+
+set(CMAKE_SYSTEM_NAME Windows)
+set(TOOLCHAIN_PREFIX x86_64-w64-mingw32)
+
+# cross compilers to use for C and C++
+set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
+set(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
+
+# target environment on the build host system
+# TODO(sako) make use check for nix using command
+# nix eval --expr '1 + 2'
+#set(CMAKE_FIND_ROOT_PATH /usr/${TOOLCHAIN_PREFIX} /usr/lib/gcc/${TOOLCHAIN_PREFIX}/7.3-posix)
+
+
+# modify default behavior of FIND_XXX() commands to
+# search for headers/libs in the target environment and
+# search for programs in the build host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
