1eebdcc2e9ec1c81484a4f21e2d0c740837693ee
Sako <78461130+Sakooooo@users.noreply.github.com>
2023-11-16 23:08:49 +0400

    brb fixing emacs

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -1,7 +1,31 @@
+#include "SDL_init.h"
+#include "SDL_log.h"
+#include "SDL_render.h"
+#include "SDL_video.h"
 #include <iostream>
 #include <SDL.h>
 
 int main() {
-  std::cout << "heloro" << std::endl;
+
+  if (!SDL_INIT_EVERYTHING) {
+    std::cout << "[CRITICAL] SDL3 FAILED TO LOAD!" << std::endl;
+    return 1;
+  };
+
+  SDL_Window* window = SDL_CreateWindow("SakoEngine DEV", 800, 800, SDL_WINDOW_OPENGL);
+
+  if (!window) {
+    SDL_Log("[CRITICAL] SDL failed to create window!");
+    return 1;
+  }
+
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL, SDL_RENDERER_ACCELERATED);
+
+  if (!renderer) {
+    SDL_Log("[CRITICAL SDL failed to create renderer!");
+    return 1;
+  };
+  
+
   return 0;
 }
