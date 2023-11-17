bee8cd09d15c2c6913e3c2b92d4330734d6ff4b4
Sako <78461130+Sakooooo@users.noreply.github.com>
2023-11-17 11:52:01 +0400

    get everything setup

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -1,3 +1,4 @@
+#include "SDL_events.h"
 #include "SDL_init.h"
 #include "SDL_log.h"
 #include "SDL_render.h"
@@ -7,17 +8,29 @@
 
 int main() {
 
+  // turn on everything
+
   if (!SDL_INIT_EVERYTHING) {
     std::cout << "[CRITICAL] SDL3 FAILED TO LOAD!" << std::endl;
     return 1;
   };
 
-  SDL_Window* window = SDL_CreateWindow("SakoEngine DEV", 800, 800, SDL_WINDOW_OPENGL);
+  // predefined height
+
+  int width = 800;
+  int height = 800;
+
+  // create window
+
+  SDL_Window* window = SDL_CreateWindow("SakoEngine DEV", width, height, SDL_WINDOW_OPENGL);
 
   if (!window) {
     SDL_Log("[CRITICAL] SDL failed to create window!");
     return 1;
   }
+
+  // renderer to display window on *NIX
+  // on Windows systems the window will show without one
 
   SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL, SDL_RENDERER_ACCELERATED);
 
@@ -25,7 +38,33 @@ int main() {
     SDL_Log("[CRITICAL SDL failed to create renderer!");
     return 1;
   };
+
+  // game loop
+
+  bool running = true;
+
+  while (running) {
+    // set the event variable
+
+    SDL_Event event;
+
+    // SDL_PollEvent uses the event variable's pointer reference (?)
+
+    //SDL_PollEvent(&event);
+
+    while (SDL_PollEvent(&event)) {
+      // Event Handling Here
+      if (event.type == SDL_EventType::SDL_EVENT_QUIT) {
+	running = false;
+      };
+    };
+    
+  };
   
 
+  // destroy renderer
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_QuitSubSystem(SDL_InitFlags::SDL_INIT_VIDEO);
   return 0;
 }
