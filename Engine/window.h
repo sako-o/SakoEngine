6d57304e2d9c44d7ff6c0a305015803361030cef
Sako <78461130+Sakooooo@users.noreply.github.com>
2023-12-03 13:05:35 +0400

    everything was breaking ????????????

--- Engine/window.h
+++ Engine/window.h
@@ -13,9 +13,6 @@ class Window {
   // decltype takes the type of function pointer decltype added in C++11
   std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> m_window; 
 
-  // TODO(sako):: Actually make this 
-  std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)> m_renderer; 
-
  public:
   // TODO(sako):: Once make default dropin, set to windowed fullscreen as default.
   explicit Window(const std::string &title, int width = 800, int height = 600, SDL_WindowFlags flags = (SDL_WindowFlags) 0); 
