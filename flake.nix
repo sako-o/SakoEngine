f66406004b02b3355d141622dffbd7f0460300f5
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-12-24 17:30:04 +0400

    this doesnt want to work

--- flake.nix
+++ flake.nix
@@ -12,9 +12,44 @@
       pkg-config
       cmake
       gnumake
-      gcc
     ];
-    buildInputs = with pkgs; [
+    sdlDeps = with pkgs; [
+      # xorg deps
+      xorg.libX11
+      xorg.xorgproto
+      xorg.libICE
+      xorg.libXi
+      xorg.libXScrnSaver
+      xorg.libXcursor
+      xorg.libXinerama
+      xorg.libXext
+      xorg.libXrandr
+      xorg.libXxf86vm
+      xorg.libXext
+      xorg.libXext.dev
+      # for audio libraries
+      pipewire
+      libpulseaudio
+      alsa-lib
+      audiofile
+      sndio
+      # drm support
+      libdrm
+      mesa
+      # graphics
+      libGL
+      vulkan-headers
+      vulkan-loader
+      vulkan-tools
+      vulkan-validation-layers
+      # wayland deps
+      wayland
+      wayland-scanner
+      # extras
+      zlib
+      libzip
+      udev
+      libiconv
     ];
     windowsNativeBuildInputs = with pkgs; [
       pkgsCross.mingwW64.buildPackages.gcc
@@ -23,7 +58,7 @@
     devShells.${system}.default = pkgs.mkShell {
       packages = [
         nativeBuildInputs
-        buildInputs
+        sdlDeps
         windowsNativeBuildInputs
       ];
       mingwW64dir = "${pkgs.pkgsCross.mingwW64.buildPackages.gcc}";
