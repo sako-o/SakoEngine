59a6416adb5e83dda21cb47262d076ce50f269b0
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-08-03 18:37:13 +0400

    make window

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdint>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
@@ -7,19 +8,43 @@
 
 // page 41 of guide
 
+const uint32_t w_width = 800;
+const uint32_t w_height = 600;
+
 class SakoEngine {
 public:
   void run() {
+    initWindow();
     initVulkan();
     mainLoop();
     cleanup();
   }
 
 private:
+  GLFWwindow *window;
+
   void initVulkan() {}
-  void initWindow() {}
-  void mainLoop() {}
-  void cleanup() {}
+  void initWindow() {
+    glfwInit();
+
+    // window hints (disable api for manually adding vulkan)
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    // disable resizeing windows
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window =
+        glfwCreateWindow(w_width, w_height, "SakoEngine", nullptr, nullptr);
+  }
+  void mainLoop() {
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+    }
+  }
+  void cleanup() {
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+  }
 };
 
 int main() {
