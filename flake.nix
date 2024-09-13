63ecaf34f87deba0407758208ac553fb2f2d0645
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-09-13 22:08:22 +0400

    i hate myself

--- flake.nix
+++ flake.nix
@@ -1,16 +1,29 @@
 {
-  description = "SakoEngine flake";
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
-  outputs = {nixpkgs, ...}: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-    shellHook = ''
-      echo ok
-    '';
+  description = "SakoEngine Flake";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      inherit (nixpkgs) lib;
+      inherit (builtins) attrValues;
+      eachSystem = f:
+        lib.genAttrs [ "x86_64-linux" "aarch64-linux" ]
+        (system: f nixpkgs.legacyPackages.${system});
+    in {
+
+      # packages = eachSystem (pkgs: rec {
+      #   default = sakoEngine;
+      #   sakoEngine = pkgs.callPackage ./package.nix { };
+      # });
+
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+         packages = attrValues { inherit (pkgs) cmake gcc; };
+        };
+      });
+
+      formatter = eachSystem (pkgs: pkgs.alejandra);
+
     };
-  };
 }
