945d22bdf74de14fa9833641f2203bf2ce9c33e7
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-09-13 22:06:02 +0400

    nuke the entire project again like a retard

--- doc/build.md
+++
@@ -1,6 +0,0 @@
-# How to build
-
-1. `git clone https://github.com/sako-o/SakoEngine.git --recursive && cd SakoEngine`
-2. `mkdir build && cd build && cmake ..`
-
-- For MinGW Makefiles make sure to add `-G 'MinGW Makefiles'`
--- flake.nix
+++ flake.nix
@@ -1,81 +1,16 @@
 {
   description = "SakoEngine flake";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
   outputs = {nixpkgs, ...}: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
-
-    nativeBuildInputs = with pkgs; [
-      clang-tools
-      pkg-config
-      cmake
-      gnumake
-    ];
-    sdlDeps = with pkgs; [
-      # xorg deps
-      # FIXME: Surely theres a better way to this? We are we adding .dev for #include to work
-      xorg.libX11
-      xorg.libX11.dev
-      xorg.xorgproto
-      xorg.libICE
-      xorg.libICE.dev
-      xorg.libXi
-      xorg.libXi.dev
-      xorg.libXScrnSaver
-      xorg.libXcursor
-      xorg.libXcursor.dev
-      xorg.libXinerama
-      xorg.libXinerama.dev
-      xorg.libXext
-      xorg.libXext.dev
-      xorg.libXrandr
-      xorg.libXrandr.dev
-      xorg.libXxf86vm
-      xorg.libXxf86vm.dev
-      xorg.libXext
-      xorg.libXext.dev
-      xorg.libxcb
-      xorg.libxcb.dev
-      xorg.xrandr
-      xorg.libXfixes
-      # for audio libraries
-      pipewire
-      libpulseaudio
-      alsa-lib
-      audiofile
-      sndio
-      # drm support
-      libdrm
-      mesa
-      # graphics
-      libGL
-      vulkan-headers
-      vulkan-loader
-      vulkan-tools
-      vulkan-validation-layers
-      # wayland deps
-      wayland
-      wayland-scanner
-      libdecor
-      # extras
-      zlib
-      libzip
-      udev
-      libiconv
-    ];
-    windowsNativeBuildInputs = with pkgs; [
-      pkgsCross.mingwW64.buildPackages.gcc
-    ];
   in {
     devShells.${system}.default = pkgs.mkShell {
-      packages = [
-        nativeBuildInputs
-        sdlDeps
-        windowsNativeBuildInputs
-      ];
-      mingwW64dir = "${pkgs.pkgsCross.mingwW64.buildPackages.gcc}";
+    shellHook = ''
+      echo ok
+    '';
     };
   };
 }
