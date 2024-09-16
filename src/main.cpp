a61eb18680f5db321c3582643509c2115986211a
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-09-16 23:02:14 +0400

    what

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
 // clang-format off
+#include <cstddef>
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 // clang-format on
 
+// ok https://learnopengl.com/Getting-started/Hello-Window
 int main() {
-  std::cout << "compiler" << std::endl;
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow *window = glfwCreateWindow(800, 600, "SakoEngine", NULL, NULL);
+
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
 
   return 0;
 }
