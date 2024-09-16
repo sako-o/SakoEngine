6c3e98ffa0c42734ff0cfab47a5e647bbaed2b2b
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-09-16 23:15:51 +0400

    remove glfw for sdl2

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
 
       devShells = eachSystem (pkgs: {
         default = pkgs.mkShell {
-          packages = attrValues { inherit (pkgs) cmake gcc glfw; inherit (pkgs.python312Packages) jinja2;};
+          packages = attrValues { inherit (pkgs) cmake gcc sdl2; };
         };
       });
 
