bb7cc6dcd1d62c6edbe2f1fa75d7a51ee7513119
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-12-23 23:03:21 +0400

    Fade away into the light

--- nix/sdl3.nix
+++ nix/sdl3.nix
@@ -1,73 +1,119 @@
 # https://github.com/NixOS/nixpkgs/pull/326699
-{ lib, stdenv, config, alsa-lib, cmake, darwin, dbus, fcitx5, fetchFromGitHub
-, ibus, installShellFiles, libGL, libdecor, libdrm, libjack2, libpulseaudio
-, libusb1, libxkbcommon, mesa, nas, ninja, nix-update-script, nixosTests
-, pipewire, pkg-config, sndio, systemdLibs, testers, validatePkgConfig, wayland
-, wayland-scanner, xorg, zenity
-, alsaSupport ? stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid
-, dbusSupport ? stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid
-, drmSupport ? stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid
-, ibusSupport ? stdenv.hostPlatform.isUnix && !stdenv.hostPlatform.isDarwin
-, jackSupport ? stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid
-, libdecorSupport ? stdenv.hostPlatform.isLinux
-  && !stdenv.hostPlatform.isAndroid
-, openglSupport ? lib.meta.availableOn stdenv.hostPlatform mesa
-, pipewireSupport ? stdenv.hostPlatform.isLinux
-  && !stdenv.hostPlatform.isAndroid, pulseaudioSupport ?
-  config.pulseaudio or stdenv.hostPlatform.isLinux
-  && !stdenv.hostPlatform.isAndroid
-, libudevSupport ? stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid
-, sndioSupport ? false, testSupport ? true
-, waylandSupport ? stdenv.isLinux && !stdenv.hostPlatform.isAndroid
-, x11Support ? !stdenv.hostPlatform.isAndroid && !stdenv.hostPlatform.isWindows,
+{
+  lib,
+  stdenv,
+  config,
+  alsa-lib,
+  apple-sdk_11,
+  cmake,
+  darwinMinVersionHook,
+  dbus,
+  fcitx5,
+  fetchFromGitHub,
+  ibus,
+  installShellFiles,
+  libGL,
+  libdecor,
+  libdrm,
+  libjack2,
+  libpulseaudio,
+  libusb1,
+  libxkbcommon,
+  mesa,
+  ninja,
+  nix-update-script,
+  nixosTests,
+  pipewire,
+  sndio,
+  systemdLibs,
+  testers,
+  validatePkgConfig,
+  wayland,
+  wayland-scanner,
+  xorg,
+  zenity,
+  alsaSupport ? stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid,
+  dbusSupport ? stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid,
+  drmSupport ? stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid,
+  ibusSupport ? stdenv.hostPlatform.isUnix && !stdenv.hostPlatform.isDarwin,
+  jackSupport ? stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid,
+  libdecorSupport ? stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid,
+  openglSupport ? lib.meta.availableOn stdenv.hostPlatform mesa,
+  pipewireSupport ? stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid,
+  pulseaudioSupport ?
+    config.pulseaudio or stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid,
+  libudevSupport ? stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid,
+  sndioSupport ? false,
+  testSupport ? true,
+  waylandSupport ? stdenv.isLinux && !stdenv.hostPlatform.isAndroid,
+  x11Support ? !stdenv.hostPlatform.isAndroid && !stdenv.hostPlatform.isWindows,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "sdl3";
   version = "3.1.6";
 
+  outputs = [
+    "lib"
+    "dev"
+    "out"
+  ];
+
   src = fetchFromGitHub {
     owner = "libsdl-org";
     repo = "SDL";
-    rev = "refs/tags/preview-${finalAttrs.version}";
+    tag = "preview-${finalAttrs.version}";
     hash = "sha256-MItZt5QeEz13BeCoLyXVUbk10ZHNyubq7dztjDq4nt4=";
   };
 
-  postPatch = ''
+  postPatch = lib.optionalString waylandSupport ''
     substituteInPlace src/video/wayland/SDL_waylandmessagebox.c \
       --replace-fail '"zenity"' '"${lib.getExe zenity}"'
   '';
 
-  outputs = [ "lib" "dev" "out" ];
-
   strictDeps = true;
 
-  nativeBuildInputs = [ cmake ninja validatePkgConfig ]
-    ++ lib.optional waylandSupport wayland-scanner;
+  nativeBuildInputs = [
+    cmake
+    ninja
+    validatePkgConfig
+  ] ++ lib.optional waylandSupport wayland-scanner;
 
-  buildInputs = finalAttrs.dlopenBuildInputs
-    ++ lib.optionals stdenv.hostPlatform.isDarwin (with darwin.apple_sdk_11_0; [
-      frameworks.AVFoundation
-      frameworks.Cocoa
-      frameworks.ForceFeedback
-      libs.xpc
-    ]) ++ lib.optionals ibusSupport [ fcitx5 ibus ]
+  buildInputs =
+    finalAttrs.dlopenBuildInputs
+    ++ lib.optionals stdenv.hostPlatform.isDarwin [
+      # error: 'MTLPixelFormatASTC_4x4_LDR' is unavailable: not available on macOS
+      (darwinMinVersionHook "11.0")
+
+      apple-sdk_11
+    ]
+    ++ lib.optionals ibusSupport [
+      fcitx5
+      ibus
+    ]
     ++ lib.optional waylandSupport zenity;
 
-  dlopenBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [
-    libusb1
-    nas # libaudio
-  ] ++ lib.optional alsaSupport alsa-lib ++ lib.optional dbusSupport dbus
+  dlopenBuildInputs =
+    lib.optionals stdenv.hostPlatform.isLinux [
+      libusb1
+    ]
+    ++ lib.optional alsaSupport alsa-lib
+    ++ lib.optional dbusSupport dbus
     ++ lib.optionals drmSupport [
       libdrm
       mesa # libgbm
-    ] ++ lib.optional jackSupport libjack2
+    ]
+    ++ lib.optional jackSupport libjack2
     ++ lib.optional libdecorSupport libdecor
     ++ lib.optional libudevSupport systemdLibs
-    ++ lib.optional openglSupport libGL ++ lib.optional pipewireSupport pipewire
+    ++ lib.optional openglSupport libGL
+    ++ lib.optional pipewireSupport pipewire
     ++ lib.optional pulseaudioSupport libpulseaudio
     ++ lib.optional sndioSupport sndio
-    ++ lib.optionals waylandSupport [ libxkbcommon wayland ]
+    ++ lib.optionals waylandSupport [
+      libxkbcommon
+      wayland
+    ]
     ++ lib.optionals x11Support [
       xorg.libX11
       xorg.libXScrnSaver
@@ -113,56 +159,60 @@ stdenv.mkDerivation (finalAttrs: {
     # helps them be found
     NIX_LDFLAGS =
       lib.optionalString (stdenv.hostPlatform.extensions.sharedLibrary == ".so")
-      "-rpath ${
-        lib.makeLibraryPath
-        (finalAttrs.dlopenBuildInputs ++ finalAttrs.dlopenPropagatedBuildInputs)
-      }";
+        "-rpath ${
+          lib.makeLibraryPath (finalAttrs.dlopenBuildInputs ++ finalAttrs.dlopenPropagatedBuildInputs)
+        }";
   };
 
   passthru = {
     # Building this in its own derivation to make sure the rpath hack above propagate to users
-    snake-example = stdenv.mkDerivation (finalAttrs': {
-      pname = "sdl3-snake-example";
+    debug-text-example = stdenv.mkDerivation (finalAttrs': {
+      pname = "sdl3-debug-text-example";
       inherit (finalAttrs) version src;
 
-      sourceRoot = "${finalAttrs'.src.name}/examples/game/01-snake";
+      sourceRoot = "${finalAttrs'.src.name}/examples/renderer/18-debug-text";
 
-      nativeBuildInputs = [ installShellFiles pkg-config ];
+      nativeBuildInputs = [
+        installShellFiles
+      ];
 
       buildInputs = [ finalAttrs.finalPackage ];
 
-      postConfigure = ''
-        appendToVar NIX_CFLAGS_COMPILE "$(pkg-config sdl3 --libs-only-l)"
-      '';
-
       postBuild = ''
-        gcc -o snake{,.c}
+        $CC -lSDL3 -o debug-text{,.c}
       '';
 
       postInstall = ''
-        installBin snake
+        installBin debug-text
       '';
 
       meta = {
         inherit (finalAttrs.meta) maintainers platforms;
-        mainProgram = "snake";
+        mainProgram = "debug-text";
       };
     });
 
-    tests = {
-      nixosTest = nixosTests.sdl3;
-      pkg-config =
-        testers.hasPkgConfigModules { package = finalAttrs.finalPackage; };
-      inherit (finalAttrs.passthru) snake-example;
-    };
+    tests =
+      {
+        pkg-config = testers.hasPkgConfigModules { package = finalAttrs.finalPackage; };
+        inherit (finalAttrs.passthru) debug-text-example;
+      }
+      // lib.optionalAttrs stdenv.hostPlatform.isLinux {
+        nixosTest = nixosTests.sdl3;
+      };
 
-    updateScript =
-      nix-update-script { extraArgs = [ "--version-regex" "'preview-(.*)'" ]; };
+    updateScript = nix-update-script {
+      extraArgs = [
+        "--version-regex"
+        "'preview-(.*)'"
+      ];
+    };
   };
 
   meta = {
     description = "Cross-platform development library (Preview version)";
     homepage = "https://libsdl.org";
+    changelog = "https://github.com/libsdl-org/SDL/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.zlib;
     maintainers = with lib.maintainers; [ getchoo ];
     platforms = lib.platforms.unix;
