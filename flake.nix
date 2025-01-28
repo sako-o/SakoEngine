ab87bcaf8c944db120f108b12a8c71f8d1f01702
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-01-28 11:54:30 +0400

    you dont wanna mess with us nix fans...

--- flake.nix
+++ flake.nix
@@ -1,34 +1,47 @@
 {
   description = "SakoEngine Flake";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+  inputs = {nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";};
 
-  outputs = { self, nixpkgs }:
-    let
-      inherit (nixpkgs) lib;
-      inherit (builtins) attrValues;
-      eachSystem = f:
-        lib.genAttrs [ "x86_64-linux" ]
-        (system: f nixpkgs.legacyPackages.${system});
-    in {
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    inherit (nixpkgs) lib;
+    inherit (builtins) attrValues;
+    eachSystem = f:
+      lib.genAttrs ["x86_64-linux"]
+      (system: f nixpkgs.legacyPackages.${system});
+  in {
+    #     packages = eachSystem (pkgs: rec {
+    # default = sakoEngine;
+    # sakoEngine = pkgs.callPackage ./package.nix { };
+    #       SDL3 = pkgs.callPackage ./nix/sdl3.nix { };
+    #     });
 
-#     packages = eachSystem (pkgs: rec {
-        # default = sakoEngine;
-        # sakoEngine = pkgs.callPackage ./package.nix { };
-#       SDL3 = pkgs.callPackage ./nix/sdl3.nix { };
-#     });
-
-      devShells = eachSystem (pkgs: {
-        default = pkgs.mkShell {
-          packages = attrValues {
-            inherit (pkgs)
-              cmake gcc gdb pkg-config sdl3 libGL glew glm vulkan-headers
-              vulkan-loader vulkan-tools;
-          };
+    devShells = eachSystem (pkgs: {
+      default = pkgs.mkShell {
+        packages = attrValues {
+          inherit
+            (pkgs)
+            cmake
+            gcc
+            gdb
+            pkg-config
+            sdl3
+            libGL
+            glew
+            glm
+            vulkan-headers
+            vulkan-loader
+            vulkan-tools
+            clang-tools
+            cmake-language-server
+            ;
         };
-      });
+      };
+    });
 
-      formatter = eachSystem (pkgs: pkgs.alejandra);
-
-    };
+    formatter = eachSystem (pkgs: pkgs.alejandra);
+  };
 }
