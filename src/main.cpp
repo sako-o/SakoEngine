f78aed8a1d02f33717eea4256fe64240cbe8a1b8
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-02-02 17:25:47 +0400

    inital vulkan setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,93 @@
+#include <cstdlib>
 #include <iostream>
+#include <vulkan/vulkan.h>
+#include <vulkan/vulkan_core.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <vector>
+
+const int width = 800;
+const int height = 600;
+
+class SakoEngine {
+public:
+  void run() {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
+  };
+
+private:
+  GLFWwindow *window;
+  VkInstance instance;
+  void initWindow() {
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(800, 600, "SakoEngine", nullptr, nullptr);
+  }
+  void createInstance() {
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "SakoEngine";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char **glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    createInfo.enabledLayerCount = 0;
+
+    // check for extensions
+    vkEnumerateInstanceExtensionProperties(nullptr, &glfwExtensionCount,
+                                           nullptr);
+    std::vector<VkExtensionProperties> extensions(glfwExtensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &glfwExtensionCount,
+                                           extensions.data());
+
+    std::cout << "available extensions:\n";
+
+    for (const auto &extension : extensions) {
+      std::cout << '\t' << extension.extensionName << '\n';
+    }
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+      throw std::runtime_error("failed to create instance!");
+    }
+  };
+  void initVulkan() { createInstance(); }
+  void mainLoop() {
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+    }
+  }
+  void cleanup() {
+    vkDestroyInstance(instance, nullptr);
+    glfwDestroyWindow(window);
+    glfwTerminate();
+  }
+};
 
 int main() {
-  std::cout << "hi" << std::endl;
-  return 0;
+  SakoEngine engine;
+
+  try {
+    engine.run();
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 }
