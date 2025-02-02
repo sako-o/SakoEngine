52ca97307be3ee96d066042acbbda4fb64c258e3
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-02-02 18:09:10 +0400

    Physical devices and queue families

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <cstring>
+#include <map>
+#include <optional>
 #include <vector>
 
 const int width = 800;
@@ -33,6 +35,12 @@ VkResult CreateDebugUtilsMessengerEXT(
     return VK_ERROR_EXTENSION_NOT_PRESENT;
   }
 }
+
+struct QueueFamilyIndices {
+  std::optional<uint32_t> graphicsFamily;
+
+  bool isComplete() { return graphicsFamily.has_value(); };
+};
 
 class SakoEngine {
 public:
@@ -196,7 +204,95 @@ private:
   void initVulkan() {
     createInstance();
     setupDebugMessenger();
+    pickPhysicalDevice();
   }
+
+  void pickPhysicalDevice() {
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+
+    uint32_t deviceCount = 0;
+    vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+    if (deviceCount == 0) {
+      throw std::runtime_error("I can't find a GPU with Vulkan support!");
+    };
+
+    std::vector<VkPhysicalDevice> devices(deviceCount);
+    vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+    for (const auto &device : devices) {
+      if (isDeviceSuitable(device)) {
+        physicalDevice = device;
+        break;
+      }
+    }
+
+    if (physicalDevice == VK_NULL_HANDLE) {
+      throw std::runtime_error("failed to find a suitable GPU!");
+    }
+  };
+  // example for setting the used graphics card automatically
+
+  // int rateDeviceSuitability(VkPhysicalDevice device) {
+  //     int score = 0;
+  //     // Discrete GPUs have a significant performance advantage
+  //     if (deviceProperties.deviceType ==
+  //     VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
+  //         score += 1000;
+  //     }
+  //     // Maximum possible size of textures affects graphics quality
+  //     score += deviceProperties.limits.maxImageDimension2D;
+  //     // Application can't function without geometry shaders
+  //     if (!deviceFeatures.geometryShader) {
+  //         return 0;
+  //     }
+  //     return score;
+  // }
+
+  bool isDeviceSuitable(VkPhysicalDevice device) {
+    // get the device properties
+    // VkPhysicalDeviceProperties deviceProperties;
+    // vkGetPhysicalDeviceProperties(device, &deviceProperties);
+    // VkPhysicalDeviceFeatures deviceFeatures;
+    // vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+
+    // return deviceProperties.deviceType ==
+    //            VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU &&
+    //        deviceFeatures.geometryShader;
+    // return true;
+    QueueFamilyIndices indices = findQueueFamilies(device);
+
+    return indices.isComplete();
+  };
+
+  QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+    QueueFamilyIndices indices;
+
+    uint32_t queueFamilyCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount,
+                                             nullptr);
+
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount,
+                                             queueFamilies.data());
+
+    int i = 0;
+    for (const auto &queueFamily : queueFamilies) {
+      if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+        indices.graphicsFamily = i;
+      }
+
+      if (indices.isComplete()) {
+        break;
+      }
+
+      i++;
+    }
+
+    // Assign index to queue families that could be found
+    return indices;
+  }
+
   void mainLoop() {
     while (!glfwWindowShouldClose(window)) {
       glfwPollEvents();
